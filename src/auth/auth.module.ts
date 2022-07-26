8c0b2ddea3ada9b6cf9517b752866ffa1cf91a07
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2022-07-26 20:10:29 +0800

    Using passport js and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [PrismaService, AuthService],
+  providers: [
+    PrismaService,
+    AuthService,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
